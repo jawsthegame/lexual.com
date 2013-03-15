f72abd088d90ba60267cc20aeb7bfd16a533edab
lexual <lex@jbadigital.com>
2013-03-15 15:11:24 +1100

    initial barebones pelican project.

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Lexual'
+SITENAME = u"Lexual's blog"
+SITEURL = ''
+
+TIMEZONE = 'Australia/Melbourne'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS = (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Pandas', 'http://pandas.pydata.org',),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('You can modify those links in your config file', '#'),
+)
+
+# Social widget
+SOCIAL = (
+    ('twitter', 'http://twitter.com/LexualChocolate',),
+    ('github', 'https://github.com/lexual',),
+)
+
+DEFAULT_PAGINATION = 10
